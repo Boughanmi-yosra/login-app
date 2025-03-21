64e6ec9188253cfaab9de9b4d5d8afb66dfa4bdf
Yosra Boughanmi <yousra.boughanmi@gmail.com>
2025-03-21 02:08:48 +0100

    fix lint issue , npm run lint -> ng lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component'; // Adjust the path as needed
 
 @Component({
